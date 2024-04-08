86844c635aed727e403c15698ecd8b2d831dc8db
Apis <159783293+APISXD@users.noreply.github.com>
2024-04-09 03:24:47 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -109,3 +109,24 @@ document.addEventListener('keydown', function(event) {
     showTable(2); // Memanggil fungsi showTable untuk menampilkan tabel selanjutnya
   }
 });
+       activeslideimg();
+
+       function activeslideimg(activeSlide = 2) {
+         const slides = document.querySelectorAll(".slide");
+
+         slides[activeSlide].classList.add("active");
+
+         for (const slide of slides) {
+           slide.addEventListener("click", () => {
+             clearActiveClasses();
+
+             slide.classList.add("active");
+           });
+         }
+
+         function clearActiveClasses() {
+           slides.forEach((slide) => {
+             slide.classList.remove("active");
+           });
+         }
+       }
